3192377856e8145dc0dc7a18916790e1b36c6dce
Imran Khan <imrankhan@teknik.io>
2020-10-16 11:40:34 +0000

    Update Hy
    Ubuntu silently upgraded python to 3.8.5 and it seems that broke
    Hy 18.0, so upgrading it to 19.0 which supports new python 3.9

--- ghdl/__init__.hy
+++ ghdl/__init__.hy
@@ -1,3 +1,3 @@
-(setv __version__ "0.2.2")
+(setv __version__ "0.2.3")
 
 (import [ghdl.main [main set-dry set-single]])
--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ package_data = \
 install_requires = \
 ['docopt>=0.6.2,<0.7.0',
  'filetype>=1.0.7,<2.0.0',
- 'hy>=0.18.0,<0.19.0',
+ 'hy>=0.19.0,<0.20.0',
  'python-dateutil>=2.8.1,<3.0.0',
  'xdg>=4.0.1,<5.0.0',
  'xtract>=0.1a3,<0.2']
 
 setup_kwargs = {
     'name': 'ghdl',
-    'version': '0.2.2',
-    'description': 'Download and keep binaries from Github releases updated',
+    'version': '0.2.3',
+    'description': 'Package manager for Github Release binaries',
     'long_description': None,
     'author': 'Imran Khan',
     'author_email': 'imrankhan@teknik.io',
